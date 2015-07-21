c31a23884cb0367938e78f9957669a9ec40407d0
Jim Tittsler <jim@oerfoundation.org>
2015-07-21 04:39:45 +0000

    Add default OERu favicon.ico
    Silences a Validator.W3.org error.

--- header.php
+++ header.php
@@ -14,7 +14,13 @@
 	<title><?php echo wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="icon" href="<?PHP echo get_theme_mod("fav_icon_url"); ?>">
+	<?php
+		$favicon_url = get_theme_mod('fav_icon_url');
+		if (trim($favicon_url) == false) {
+			$favicon_url = get_template_directory_uri() . '/favicon.ico';
+		}
+	?>
+	<link rel="icon" href="<?PHP echo $favicon_url; ?>">
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
