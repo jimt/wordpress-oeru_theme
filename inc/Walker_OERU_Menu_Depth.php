c12636adc17f27ad755ecb3c91cf5f7ab3853983
Jim Tittsler <jimt@onjapan.net>
2015-11-12 05:49:17 +0000

    Fix menu when only showing 2 levels.
    When showing only 2 levels in the top dropdown menu, 2nd level
    menu items with children need to "skip ahead" to the first child
    item since the 2nd level item wouldn't have a page of its own.

--- inc/Walker_OERU_Menu_Depth.php
+++ inc/Walker_OERU_Menu_Depth.php
@@ -2,13 +2,13 @@
 
 class Walker_OERU_Menu_Depth extends Walker {
 
-    // Tell Walker where to inherit it's parent and id values
-    var $db_fields = array(
-        'parent' => 'menu_item_parent', 
-        'id'     => 'db_id' 
-    );
+	// Tell Walker where to inherit its parent and id values
+	var $db_fields = array(
+		'parent' => 'menu_item_parent',
+		'id'     => 'db_id'
+	);
 	
-	var $first = false;
+	var $fixup = false;
 
     
 	function start_lvl( &$output, $depth = 0, $args = array() ) {
@@ -32,7 +32,6 @@ class Walker_OERU_Menu_Depth extends Walker {
 	}
 	
 	function end_el( &$output, $object, $depth = 0, $args = array() ) {
-	
 		if($depth<=0){
 	
 			$output .= "</li>";
@@ -42,61 +41,69 @@ class Walker_OERU_Menu_Depth extends Walker {
 	}
 	
     function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-	
 		if($depth<=1){
 	
-		$page_class = "";
-		
-		foreach($item->classes as $index => $class){
-		
-			if(strpos($class,"-")===FALSE){
+			$page_class = "";
 			
-				$page_class .= $class . " ";
-				unset($item->classes[$index]);
+			foreach($item->classes as $index => $class){
+
+				if(strpos($class,"-")===FALSE){
+
+					$page_class .= $class . " ";
+					unset($item->classes[$index]);
+
+				}
 			
 			}
-		
-		}
-		
-		$item->classes[] = str_replace(" ", "-", strtolower(trim($page_class)));
-	
-		if(in_array("menu-item-has-children",$item->classes)){
-			$item->classes[] = "dropdown";
-		}
-		
-		if($item->menu_item_parent!=0){
-			$item->classes[] = "sub-menu";
-		}else{
-			$item->classes[] = "parent-menu";
-		}
-		
-		if($depth !=0){
-			if(($key = array_search("menu-item-has-children", $item->classes)) !== false) {
-				unset($item->classes[$key]);
-			}
-		}
-	
-        $output .= "<li class='" . implode(" ", $item->classes) . "'><a ";
-		if ( $item->object_id === get_the_ID() ) {
-			$output .= " class='current' ";
-		}
-		
-		if($depth == 0){
-			$output .= " href='#' data-toggle='dropdown' class='dropdown-toggle'>" . $item->title;
-		}else{
-			$output .= " href='" . $item->url . "'>" . $item->title;
-		}
-	
-		if($depth==0){
+
+			$item->classes[] = str_replace(" ", "-", strtolower(trim($page_class)));
 		
 			if(in_array("menu-item-has-children",$item->classes)){
-				$output .= "<span class='dropdown caret'></span>";	
+				$item->classes[] = "dropdown";
 			}
+
+			if($item->menu_item_parent!=0){
+				$item->classes[] = "sub-menu";
+			}else{
+				$item->classes[] = "parent-menu";
+			}
+
+			if($depth !=0){
+				if(($key = array_search("menu-item-has-children", $item->classes)) !== false) {
+					unset($item->classes[$key]);
+				}
+			}
+
+			$output .= "<li class='" . implode(" ", $item->classes) . "'><a ";
+			if ( $item->object_id === get_the_ID() ) {
+				$output .= " class='current' ";
+			}
+
+			if($depth == 0){
+				$output .= " href='#' data-toggle='dropdown' class='dropdown-toggle'>" . $item->title;
+			}else{
+				if ( in_array( 'dropdown', $item->classes ) ) {
+					$output .= ' href="-OERU--SECOND-LEVEL-MENU">' . $item->title;
+					$fixup = true;
+				} else {
+					$output .= " href='" . $item->url . "'>" . $item->title;
+				}
+			}
+
+			if($depth==0){
+
+				if(in_array("menu-item-has-children",$item->classes)){
+					$output .= "<span class='dropdown caret'></span>";
+				}
+
+			}
+
+			$output .=  "</a>";
 		
-		}
-		
-		$output .=  "</a>";
-		
+		} else if ( fixup && ( $depth == 2 ) ) {
+			// backfill the last first second level URL with the first third level one
+			$output = str_replace( '-OERU--SECOND-LEVEL-MENU', $item->url, $output );
+			$fixup = false;
 		}
 		
     }
